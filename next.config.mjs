f864abe64c02a8df291c6e5376234d07f37535fb
bruceJenner <brucejenner.sol@proton.me>
2024-10-16 23:46:22 +0200

    adds proper generatio of prisma client (@vercel/next & scrips: prisma generate

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import { withVercelPrisma } from '@vercel/next';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withVercelPrisma({
+  // Your existing config can go here
+});
 
 export default nextConfig;
